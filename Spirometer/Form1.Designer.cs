b6d81b48418eb9eae913bca90b0c657043585a7f
wangkun <wangkun@cmiot.chinamobile.com>
2020-01-08 16:42:19 +0800

    完成整体布局

--- Spirometer/Form1.Designer.cs
+++ Spirometer/Form1.Designer.cs
@@ -28,13 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.plotViewCV = new OxyPlot.WindowsForms.PlotView();
+            this.plotViewCT = new OxyPlot.WindowsForms.PlotView();
+            this.plotViewVT = new OxyPlot.WindowsForms.PlotView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.plotViewVT);
+            this.splitContainer1.Size = new System.Drawing.Size(1372, 550);
+            this.splitContainer1.SplitterDistance = 325;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.plotViewCV);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.plotViewCT);
+            this.splitContainer2.Size = new System.Drawing.Size(1372, 325);
+            this.splitContainer2.SplitterDistance = 668;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // plotViewCV
+            // 
+            this.plotViewCV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plotViewCV.Location = new System.Drawing.Point(0, 0);
+            this.plotViewCV.Name = "plotViewCV";
+            this.plotViewCV.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotViewCV.Size = new System.Drawing.Size(664, 321);
+            this.plotViewCV.TabIndex = 0;
+            this.plotViewCV.Text = "plotView1";
+            this.plotViewCV.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotViewCV.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotViewCV.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // plotViewCT
+            // 
+            this.plotViewCT.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plotViewCT.Location = new System.Drawing.Point(0, 0);
+            this.plotViewCT.Name = "plotViewCT";
+            this.plotViewCT.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotViewCT.Size = new System.Drawing.Size(696, 321);
+            this.plotViewCT.TabIndex = 0;
+            this.plotViewCT.Text = "plotView1";
+            this.plotViewCT.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotViewCT.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotViewCT.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // plotViewVT
+            // 
+            this.plotViewVT.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plotViewVT.Location = new System.Drawing.Point(0, 0);
+            this.plotViewVT.Name = "plotViewVT";
+            this.plotViewVT.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotViewVT.Size = new System.Drawing.Size(1368, 217);
+            this.plotViewVT.TabIndex = 0;
+            this.plotViewVT.Text = "plotView1";
+            this.plotViewVT.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotViewVT.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotViewVT.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1372, 550);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private OxyPlot.WindowsForms.PlotView plotViewCV;
+        private OxyPlot.WindowsForms.PlotView plotViewCT;
+        private OxyPlot.WindowsForms.PlotView plotViewVT;
     }
 }
 
--- Spirometer/Form1.cs
+++ Spirometer/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,166 @@ namespace Spirometer
 {
     public partial class Form1 : Form
     {
+        private PlotModel m_plotModelCV; // 容积-流量图Model
+        private PlotModel m_plotModelCT; // 容积-时间图Model
+        private PlotModel m_plotModelVT; // 流量-时间图Model
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /* 容积-流量图 */
+            m_plotModelCV = new PlotModel()
+            {
+                Title = "容积-流量图",
+                LegendTitle = "图例",
+                LegendOrientation = LegendOrientation.Horizontal,
+                LegendPlacement = LegendPlacement.Inside,
+                LegendPosition = LegendPosition.TopRight,
+                LegendBackground = OxyColors.Beige,
+                LegendBorder = OxyColors.Black
+            };
+
+            //X轴
+            var xAxisCV = new LinearAxis()
+            {
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                IsZoomEnabled = true,
+                IsPanEnabled = true,
+                Position = AxisPosition.Bottom,
+                Minimum = 0,
+                Maximum = 1000
+            };
+            m_plotModelCV.Axes.Add(xAxisCV);
+
+            //Y轴
+            var yAxisCV = new LinearAxis()
+            {
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                IsZoomEnabled = true,
+                IsPanEnabled = true,
+                Position = AxisPosition.Left
+            };
+            m_plotModelCV.Axes.Add(yAxisCV);
+
+            // 原始数据
+            var seriesCV = new LineSeries()
+            {
+                Color = OxyColors.Green,
+                StrokeThickness = 1,
+                MarkerSize = 1,
+                MarkerStroke = OxyColors.DarkGreen,
+                MarkerType = MarkerType.Circle,
+                Title = "Data"
+            };
+            m_plotModelCV.Series.Add(seriesCV);
+
+            plotViewCV.Model = m_plotModelCV;
+
+            /* 容积-时间图 */
+            m_plotModelCT = new PlotModel()
+            {
+                Title = "容积-时间图",
+                LegendTitle = "图例",
+                LegendOrientation = LegendOrientation.Horizontal,
+                LegendPlacement = LegendPlacement.Inside,
+                LegendPosition = LegendPosition.TopRight,
+                LegendBackground = OxyColors.Beige,
+                LegendBorder = OxyColors.Black
+            };
+
+            //X轴
+            var xAxisCT = new LinearAxis()
+            {
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                IsZoomEnabled = true,
+                IsPanEnabled = true,
+                Position = AxisPosition.Bottom,
+                Minimum = 0,
+                Maximum = 1000
+            };
+            m_plotModelCT.Axes.Add(xAxisCT);
+
+            //Y轴
+            var yAxisCT = new LinearAxis()
+            {
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                IsZoomEnabled = true,
+                IsPanEnabled = true,
+                Position = AxisPosition.Left
+            };
+            m_plotModelCT.Axes.Add(yAxisCT);
+
+            // 原始数据
+            var seriesCT = new LineSeries()
+            {
+                Color = OxyColors.Green,
+                StrokeThickness = 1,
+                MarkerSize = 1,
+                MarkerStroke = OxyColors.DarkGreen,
+                MarkerType = MarkerType.Circle,
+                Title = "Data"
+            };
+            m_plotModelCT.Series.Add(seriesCT);
+
+            plotViewCT.Model = m_plotModelCT;
+
+            /* 流量-时间图 */
+            m_plotModelVT = new PlotModel()
+            {
+                Title = "流量-时间图",
+                LegendTitle = "图例",
+                LegendOrientation = LegendOrientation.Horizontal,
+                LegendPlacement = LegendPlacement.Inside,
+                LegendPosition = LegendPosition.TopRight,
+                LegendBackground = OxyColors.Beige,
+                LegendBorder = OxyColors.Black
+            };
+
+            //X轴
+            var xAxisVT = new LinearAxis()
+            {
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                IsZoomEnabled = true,
+                IsPanEnabled = true,
+                Position = AxisPosition.Bottom,
+                Minimum = 0,
+                Maximum = 1000
+            };
+            m_plotModelVT.Axes.Add(xAxisVT);
+
+            //Y轴
+            var yAxisVT = new LinearAxis()
+            {
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                IsZoomEnabled = true,
+                IsPanEnabled = true,
+                Position = AxisPosition.Left
+            };
+            m_plotModelVT.Axes.Add(yAxisVT);
+
+            // 原始数据
+            var seriesVT = new LineSeries()
+            {
+                Color = OxyColors.Green,
+                StrokeThickness = 1,
+                MarkerSize = 1,
+                MarkerStroke = OxyColors.DarkGreen,
+                MarkerType = MarkerType.Circle,
+                Title = "Data"
+            };
+            m_plotModelVT.Series.Add(seriesVT);
+
+            plotViewVT.Model = m_plotModelVT;
         }
     }
 }
