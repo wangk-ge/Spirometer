116ab45789cbe3b8bfb2677f2c038729248c5e5f
wangkun <wangkun@cmiot.chinamobile.com>
2020-04-13 23:11:02 +0800

    1、统一行尾为\r\n;2、优化滑动均值滤波器效率;

--- PulmonaryFunctionLib/RollingAverageFilter.cs
+++ PulmonaryFunctionLib/RollingAverageFilter.cs
@@ -7,31 +7,94 @@ namespace PulmonaryFunctionLib
     class RollingAverageFilter
     {
         private readonly int WINDOW_SIZE; // 滑动窗口大小
-        private Queue<double> m_filterWindowQue;
+        //private Queue<double> m_filterWindowQue;
+        private double[] m_filterWindowQue; // 滑动窗口队列
+        private int m_queHead = 0; // 队列头
+        private int m_queTail = 0; // 队列尾
+        private int m_queLen = 0; // 队列长度
+
+        private double m_dataSum = 0.0; // 数据求和
 
         public RollingAverageFilter(int windowSize)
         {
 			WINDOW_SIZE = windowSize;
-            m_filterWindowQue = new Queue<double>(WINDOW_SIZE);
+            //m_filterWindowQue = new Queue<double>(WINDOW_SIZE);
+            m_filterWindowQue = new double[WINDOW_SIZE];
+            ClearQue();
+            m_dataSum = 0.0;
+        }
+
+        /* 清空队列 */
+        private void ClearQue()
+        {
+            m_queHead = 0;
+            m_queTail = 0;
+            m_queLen = 0;
+        }
+
+        /* 队列长度 */
+        private int QueLen()
+        {
+            return m_queLen;
+        }
+
+        /* 入队 */
+        private void Enqueue(double data)
+        {
+            if (m_queLen >= m_filterWindowQue.Length)
+            { // 已满
+                return;
+            }
+
+            m_filterWindowQue[m_queHead] = data;
+            ++m_queHead;
+            if (m_queHead >= m_filterWindowQue.Length)
+            {
+                m_queHead = 0;
+            }
+            ++m_queLen;
+        }
+
+        /* 出队 */
+        private double Dequeue()
+        {
+            if (m_queLen <= 0)
+            { // 已空
+                return 0.0;
+            }
+
+            double data = m_filterWindowQue[m_queTail];
+            ++m_queTail;
+            if (m_queTail >= m_filterWindowQue.Length)
+            {
+                m_queTail = 0;
+            }
+            --m_queLen;
+
+            return data;
+        }
+
+        /* 重置滤波器 */
+        public void Reset()
+        {
+            ClearQue();
+            m_dataSum = 0.0;
         }
 
         /* 执行滤波 */
         public double Input(double val)
         {
             /* 更新滑动窗口 */
-            if (m_filterWindowQue.Count >= WINDOW_SIZE)
+            if (QueLen() >= WINDOW_SIZE)
             {
-                m_filterWindowQue.Dequeue();
+                double data = Dequeue();
+                m_dataSum -= data;
             }
-            m_filterWindowQue.Enqueue(val);
+            Enqueue(val);
+            m_dataSum += val;
 
             /* 计算窗口内样本的平均值 */
-            double sum = 0.0;
-            foreach (var data in m_filterWindowQue)
-            {
-                sum += data;
-            }
-            double avg = sum / m_filterWindowQue.Count;
+            double avg = m_dataSum / QueLen();
 
             return avg;
         }
